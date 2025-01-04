c02ac78a2a8e99c711ad00566ee1a9152539249f
Xavier Larrea <jx@jxl.me>
2025-01-04 02:10:23 -0500

    Refactor sync.sh

--- sync.sh
+++ sync.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Get the directory of the current script
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-# Define the path to the subscript (relative to the script directory)
 SUBSCRIPT_DIR="$SCRIPT_DIR/includes/custom_formats"
 SUBSCRIPT="$SUBSCRIPT_DIR/update.sh"
 
-# Check if the subscript exists
 if [[ -f "$SUBSCRIPT" ]]; then
     echo "Running subscript: $SUBSCRIPT"
-    # Run the subscript
     cd "$SUBSCRIPT_DIR"
     bash "$SUBSCRIPT"
 else
@@ -18,7 +14,6 @@ else
     exit 1
 fi
 
-# Run the "recyclarr sync" command after the subscript
 echo "Running 'recyclarr sync'..."
 
 recyclarr sync
