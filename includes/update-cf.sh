4c150c80e9e6ea2d2cb9b8334ac49691194c3fd0
Xavier Larrea <jx@jxl.me>
2025-01-03 19:24:12 -0500

    Fixes

--- includes/update-cf.sh
+++ includes/update-cf.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
- 
-# Removing old files
+
+# Removing old file
 echo "Deleting old file..."
-rm radarr-new-cf.yml
-rm sonarr-new-cf.yml
-echo "Done"
-# Download latest fustom formats from Trash Guides
+rm -f radarr-new-cf.yml
+rm -f sonarr-new-cf.yml
+echo "Done."
+
+# Download latest custom formats from Trash Guides
 echo "Downloading latest Custom Formats..."
 recyclarr list custom-formats radarr --raw >> radarr-new-cf.yml
 recyclarr list custom-formats sonarr --raw >> sonarr-new-cf.yml
 echo "Done."
 
-#Fix formatting
+# Fix formatting
 echo "Fixing formatting..."
 sed -e '/^Muxers)/ s/^#*/#/' -i radarr-new-cf.yml
 sed -e '/^Muxers)/ s/^#*/#/' -i sonarr-new-cf.yml
@@ -34,9 +35,8 @@ done < "radarr-all-cf-exclusions.txt"
 
 # Comment out unneeded custom formats
 echo "Commenting out uneeded Radarr custom formats..."
-for i in "${arrRadarr[@]}"
-do
-   echo "Radarrr: $i"
+for i in "${arrRadarr[@]}"; do
+   echo "Radarr: $i"
    sed -e "/$i/ s/^#*/#/" -i radarr-new-cf.yml
 done
 echo "Done."
